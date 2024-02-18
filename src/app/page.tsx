78f753e4bf8c750b627e46171d4654a5bd70120c
ricokurnia12 <ricokurnia19@gmail.com>
2024-02-18 23:45:50 +0700

    finishing redux hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,32 @@
 'use client'
 import React,{useState} from 'react';
 import store from '@/redux/store';
-import { connect } from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import { addTodo, deleteTodo, editTodo, finishTodo } from '@/redux/actions/todoActions';
 import InputTodo from "./component/InputTodo";
 import ListDone from "./component/ListDone";
 import ListTodo from "./component/ListTodo";
 
-function Home({ todoLists, taskDone, addTodo, deleteTodo, editTodo, finishTodo }) {
+export default function Home() {
   const [inputTodo,setInputTodo]= useState<string | undefined>()
+  const dispatch = useDispatch()
+  const handleAddTodo = ()=>{
+dispatch(addTodo(inputTodo))
+  }
+  const todoLists = useSelector(state=>state.todoLists);
+
+  
+  const taskDone = useSelector(state => state.taskDone);
   return (
    
       <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
-      <InputTodo  submit={() => addTodo(inputTodo)}  inputChange={(e)=>setInputTodo(e.target.value)} />
+      <InputTodo submit={handleAddTodo} inputChange={(e)=>setInputTodo(e.target.value)} />
       <div className="grid grid-cols-12 w-full gap-4">
         <ListTodo
           data={todoLists}
-          handleDelete={(index) => deleteTodo(index)}
-          handleEdit={(index, value) => editTodo(index, value)}
-          handleDone={(index) => finishTodo(index)}
+          handleDelete={(index) => dispatch(deleteTodo(index))}
+          handleEdit={(index, value) => dispatch(editTodo(index, value))}
+          handleDone={(index) => dispatch(finishTodo(index))}
         />
         <ListDone data={taskDone} />
       </div>
@@ -28,18 +36,4 @@ function Home({ todoLists, taskDone, addTodo, deleteTodo, editTodo, finishTodo }
   );
 }
 
-const mapStateToProps = (state) => ({
 
-  
-  todoLists: state.todoLists,
-  taskDone: state.taskDone,
-});
-
-const mapDispatchToProps = {
-  addTodo,
-  deleteTodo,
-  editTodo,
-  finishTodo,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
